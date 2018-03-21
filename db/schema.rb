88120773e6fce90479e067053e64c52f42d7eb35
CFV <cristian.fraile@morningstar.com>
2018-03-20 19:05:42 -0500

    add description column to people

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317002417) do
+ActiveRecord::Schema.define(version: 20180320235352) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180317002417) do
     t.boolean "genero"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "vehicles", force: :cascade do |t|
