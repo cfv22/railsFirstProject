c8146ea987af173fbdcb8de100806c885e2c72c4
CFV <cristian.fraile@morningstar.com>
2018-03-21 18:42:22 -0500

    add description

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,23 @@ class PeopleController < ApplicationController
   def indez
     @people = Person.all 
   end
+  def show
+    @person = Person.find(params[:id])
+  end
+  def edit
+   @ppl = Person.find(params[:id])
+  end 
+  def update
+  @ppl = Person.find(params[:id])
+  if @ppl.update(person_params)
+    redirect_to people_show_path(@ppl.id)
+  else
+    render :edit
+  end
+  end 
+  #strong params
+  private
+  def person_params 
+    params.require(:person).permit(:name , :edad, :genero, :description, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   
 
-  get 'people/indez'
+  get 'people', to: 'people#indez'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'home#show'
+  get 'people/:id', to: 'people#show', as: 'people_show' 
   get '/name/:name/:deudor', to: 'home#name'
   get 'name/registro', to: 'home#registro', as: 'registro'
+  get 'people/:id/edit', to: 'people#edit', as: 'people_edit'
+  patch 'people/:id', to: 'people#update'
 end
